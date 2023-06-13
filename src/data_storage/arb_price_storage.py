4516f9f45ec1e2846bca9d1f793fd28cf4f21a50
zhant09 <zhantao92@gmail.com>
2023-06-13 18:54:19 +0800

    arb storage

---
+++ src/data_storage/arb_price_storage.py
@@ -0,0 +1,54 @@
+import gzip
+import logging
+import os
+import time
+
+from web3 import Web3
+
+from utils.config import conf, BASE_PATH
+from utils import math_utils
+from utils import arb_abi
+
+
+# ARB 0.05% USDC/ETH pool
+POOL_ADDRESS = "0xC31E54c7a869B9FcBEcc14363CF510d1c41fa443"  
+
+
+class GZipRotator:
+    def __call__(self, source, dest):
+        os.rename(source, dest)
+        f_in = open(dest, 'rb')
+        f_out = gzip.open("%s.gz" % dest, 'wb')
+        f_out.writelines(f_in)
+        f_out.close()
+        f_in.close()
+        os.remove(dest)
+
+log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+log = logging.handlers.TimedRotatingFileHandler(BASE_PATH + '/logs/' + 'arb_price_storage.log', 'midnight', 1, backupCount=30)
+log.setLevel(logging.INFO)
+log.setFormatter(log_formatter)
+log.rotator = GZipRotator()
+
+logger = logging.getLogger(__name__)
+logger.addHandler(log)
+
+
+def main():
+    arbitrum_url = conf.get_config('INFURA', 'arb_url')
+    logger.info('Starting Arb Price Storage')
+    web3 = Web3(Web3.HTTPProvider(arbitrum_url))
+    pool_contract = web3.eth.contract(address=POOL_ADDRESS, abi=arb_abi)
+    
+    while True:
+        start_time = int(time.time() * 1000)
+        slot0 = pool_contract.functions.slot0().call()
+        end_time = int(time.time() * 1000)
+        sqrt_price = slot0[0]
+        tick = slot0[1]
+        logger.info("start_time: %s, tick: %s, tick_price: %s, sqrt_price: %s, time_cost: %s", start_time, tick, 
+                    math_utils.tick_to_price(tick), math_utils.sqrt_price_normalize(sqrt_price), end_time - start_time)
+        
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils/math_utils.py
@@ -0,0 +1,12 @@
+import math
+
+TICK_BASE = 1.0001
+
+def tick_to_price(tick):
+    return TICK_BASE ** tick * 1e12
+
+def price_to_tick(price):
+    return math.log(price / 1e12, TICK_BASE)
+
+def sqrt_price_normalize(sqrt_price):
+    return (sqrt_price / (1 << 96)) ** 2 * 1e12
