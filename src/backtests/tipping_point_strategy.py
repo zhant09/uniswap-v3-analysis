160d867d2feb88e9b45deab94df10fe0ba85c61b
zhant09 <zhantao92@gmail.com>
2023-07-15 10:35:00 +0800

    fix

--- src/backtests/tipping_point_strategy.py
+++ src/backtests/tipping_point_strategy.py
@@ -21,8 +21,8 @@ from evaluation import sharpe_ratio
 
 class TippingPointStrategy(object):
 
-    FILE_PATH = BASE_PATH + "/data/eth_usd_20230710.csv"
-    # FILE_PATH = BASE_PATH + "/data/eth_usd_polygon_20230712.csv"
+    # FILE_PATH = BASE_PATH + "/data/eth_usd_20230710.csv"
+    FILE_PATH = BASE_PATH + "/data/eth_usd_polygon_20230712.csv"
 
     def __init__(self, init_usd, trade_amount, fee_rate, is_train=False, is_test=False):
         self.trade_amount = trade_amount
@@ -32,24 +32,23 @@ class TippingPointStrategy(object):
         self.trade_history = []  # (daytime, price, amount, "S"/"B")
         self.max_drawdown_list = []
         self.cost = 0
-        self.last_buy_value = 0
         self.last_buy_price = 0
 
     def _init_data(self, is_train, is_test):
         train_start = "2022-06-01"
         train_end = "2023-03-21"
-        # if is_train:
-        #     self.data = price_data_parser.parse_polygon_data(self.FILE_PATH, train_start, train_end)
-        # elif is_test:
-        #     self.data = price_data_parser.parse_polygon_data(self.FILE_PATH, train_end)
-        # else:
-        #     self.data = price_data_parser.parse_polygon_data(self.FILE_PATH, train_start)
         if is_train:
-            self.data = price_data_parser.parse_yahoo_data(self.FILE_PATH, train_start, train_end)
+            self.data = price_data_parser.parse_polygon_data(self.FILE_PATH, train_start, train_end)
         elif is_test:
-            self.data = price_data_parser.parse_yahoo_data(self.FILE_PATH, train_end)
+            self.data = price_data_parser.parse_polygon_data(self.FILE_PATH, train_end)
         else:
-            self.data = price_data_parser.parse_yahoo_data(self.FILE_PATH, train_start)
+            self.data = price_data_parser.parse_polygon_data(self.FILE_PATH, train_start)
+        # if is_train:
+        #     self.data = price_data_parser.parse_yahoo_data(self.FILE_PATH, train_start, train_end)
+        # elif is_test:
+        #     self.data = price_data_parser.parse_yahoo_data(self.FILE_PATH, train_end)
+        # else:
+        #     self.data = price_data_parser.parse_yahoo_data(self.FILE_PATH, train_start)
 
     def _on_sell(self, daytime, sell_price, amount):
         try:
@@ -67,7 +66,6 @@ class TippingPointStrategy(object):
             self.trade_history.append((daytime, sell_price, self.position.current_eth, "S"))
             self.cost = 0
             self.last_buy_price = 0
-            # self.last_buy_value = 0
 
             self.print_trade_result(daytime, sell_price, "S")
             max_drawdown_data = self.max_drawdown_list[-1]
@@ -83,7 +81,6 @@ class TippingPointStrategy(object):
             self.trade_history.append((daytime, buy_price, amount, "B"))
             self.cost = (self.cost * (self.position.current_eth - amount) + amount * buy_price) / self.position.current_eth
             self.last_buy_price = buy_price
-            # self.last_buy_value = self.position.current_eth * self.cost + self.position.current_usd
             self.print_trade_result(daytime, buy_price, "B")
         except Exception as e:
             print(daytime, e)
@@ -92,11 +89,11 @@ class TippingPointStrategy(object):
     def _is_period_lowest(self, daytime, price, period):
         is_lowest = True
         # yahoo data
-        start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d") - datetime.timedelta(days=period)).strftime(
-            "%Y-%m-%d")
+        # start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d") - datetime.timedelta(days=period)).strftime(
+        #     "%Y-%m-%d")
         # polygon data
-        # start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d %H:%M:%S") - datetime.timedelta(days=period)
-        #                   ).strftime("%Y-%m-%d %H:%M:%S")
+        start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d %H:%M:%S") - datetime.timedelta(days=period)
+                          ).strftime("%Y-%m-%d %H:%M:%S")
         for d in self.data:
             current_daytime = d["datetime"]
             current_price = d["price"]
@@ -111,11 +108,11 @@ class TippingPointStrategy(object):
 
     def _get_period_highest_price(self, daytime, period):
         # yahoo data
-        start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d") - datetime.timedelta(days=period)).strftime(
-            "%Y-%m-%d")
+        # start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d") - datetime.timedelta(days=period)).strftime(
+        #     "%Y-%m-%d")
         # polygon data
-        # start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d %H:%M:%S") - datetime.timedelta(days=period)
-        #                   ).strftime("%Y-%m-%d %H:%M:%S")
+        start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d %H:%M:%S") - datetime.timedelta(days=period)
+                          ).strftime("%Y-%m-%d %H:%M:%S")
         highest_price = 0
         for d in self.data:
             current_daytime = d["datetime"]
@@ -134,20 +131,19 @@ class TippingPointStrategy(object):
 
     def calculate_drawdown(self, price):
         return self.position.current_eth * price - self.position.current_eth * self.cost
-        # return self.position.current_eth * price + self.position.current_usd - self.last_buy_value
 
     def main(self, period):
         position_day_cnt = 0
         buy_cnt = 0
         sell_cnt = 0
 
-        # yahoo data
         max_drawdown = 0
         max_drawdown_rate = 0
         max_drawdown_price = 0
-        for d in self.data[period:]:
+        # yahoo data
+        # for d in self.data[period:]:
         # polygon data
-        # for d in self.data[period * 24:]:
+        for d in self.data[period * 24:]:
             daytime = d["datetime"]
             price = d["price"]
 
