f97a407597233a70e1545ea8b6b00d41d437e348
zhant09 <zhantao92@gmail.com>
2023-07-17 16:12:25 +0800

    fix path

--- src/backtests/tipping_point_strategy.py
+++ src/backtests/tipping_point_strategy.py
@@ -22,15 +22,14 @@ from entity.uniswap_trade import Trade, TradeType
 
 class TippingPointStrategy(object):
 
-    # FILE_PATH = BASE_PATH + "/data/eth_usd_20230710.csv"
-    FILE_PATH = BASE_PATH + "/data/eth_usd_polygon_20230714.csv"
-
     BUY_LIMIT = 1800
 
-    def __init__(self, init_usd, trade_amount, fee_rate, is_train=False, is_test=False):
+    def __init__(self, init_usd, trade_amount, fee_rate, file_path, is_polygon_data):
         self.trade_amount = trade_amount
         self.fee_rate = fee_rate
-        self._init_data(is_train, is_test)
+        self.is_polygon = is_polygon_data
+
+        self._init_data(file_path)
         self.position = Position(0, init_usd, self.fee_rate)
         self.trade_history = []  # (daytime, price, amount, "S"/"B")
         self.max_drawdown_list = []
@@ -39,21 +38,11 @@ class TippingPointStrategy(object):
         self.sell_cnt = 0
         self.last_buy_price = 0
 
-    def _init_data(self, is_train, is_test):
-        train_start = "2022-06-01"
-        train_end = "2023-03-21"
-        if is_train:
-            self.data = price_data_parser.parse_polygon_data(self.FILE_PATH, train_start, train_end)
-        elif is_test:
-            self.data = price_data_parser.parse_polygon_data(self.FILE_PATH, train_end)
+    def _init_data(self, file_path):
+        if self.is_polygon:
+            self.data = price_data_parser.parse_polygon_data(file_path, "2022-06-01 00:00:00")
         else:
-            self.data = price_data_parser.parse_polygon_data(self.FILE_PATH, train_start)
-        # if is_train:
-        #     self.data = price_data_parser.parse_yahoo_data(self.FILE_PATH, train_start, train_end)
-        # elif is_test:
-        #     self.data = price_data_parser.parse_yahoo_data(self.FILE_PATH, train_end)
-        # else:
-        #     self.data = price_data_parser.parse_yahoo_data(self.FILE_PATH, train_start)
+            self.data = price_data_parser.parse_yahoo_data(file_path, "2022-06-01")
 
     def _on_sell(self, daytime, sell_price, amount):
         try:
@@ -97,14 +86,20 @@ class TippingPointStrategy(object):
             print(daytime, e)
             return False
 
+    def _get_period_start(self, end_datetime, period):
+        # polygon data
+        if self.is_polygon:
+            start_datetime = (datetime.datetime.strptime(end_datetime, "%Y-%m-%d %H:%M:%S") - datetime.timedelta(
+                days=period)).strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            start_datetime = (datetime.datetime.strptime(end_datetime, "%Y-%m-%d") - datetime.timedelta(
+                days=period)).strftime("%Y-%m-%d")
+        return start_datetime
+
     def _is_period_lowest(self, daytime, price, period):
         is_lowest = True
-        # yahoo data
-        # start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d") - datetime.timedelta(days=period)).strftime(
-        #     "%Y-%m-%d")
-        # polygon data
-        start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d %H:%M:%S") - datetime.timedelta(days=period)
-                          ).strftime("%Y-%m-%d %H:%M:%S")
+        start_datetime = self._get_period_start(daytime, period)
+
         for d in self.data:
             current_daytime = d["datetime"]
             current_price = d["price"]
@@ -119,11 +114,8 @@ class TippingPointStrategy(object):
 
     def _get_period_highest_price(self, daytime, period):
         # yahoo data
-        # start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d") - datetime.timedelta(days=period)).strftime(
-        #     "%Y-%m-%d")
-        # polygon data
-        start_datetime = (datetime.datetime.strptime(daytime, "%Y-%m-%d %H:%M:%S") - datetime.timedelta(days=period)
-                          ).strftime("%Y-%m-%d %H:%M:%S")
+        start_datetime = self._get_period_start(daytime, period)
+
         highest_price = 0
         for d in self.data:
             current_daytime = d["datetime"]
@@ -149,10 +141,12 @@ class TippingPointStrategy(object):
         sell_cnt = 0
 
         max_drawdown_tuple = (0, 0, 0) # drawdown, price, drawdown_rate
-        # yahoo data
-        # for d in self.data[period:]:
-        # polygon data
-        for d in self.data[period * 24:]:
+
+        start = period
+        if self.is_polygon:
+            start = period * 24
+
+        for d in self.data[start:]:
             daytime = d["datetime"]
             price = d["price"]
 
@@ -200,20 +194,24 @@ class TippingPointStrategy(object):
             buy_cnt += 1
             print("highest_price:", highest_price, "price:", price, "diff rate:", diff_rate)
 
-        position_day_rate = position_day_cnt / len(self.data) - period
+        position_rate = position_day_cnt / (len(self.data) - start)
         # ploygon data
-        position_day_cnt /= 24
+        if self.is_polygon:
+            position_day_cnt /= 24
         print("buy cnt:", buy_cnt, "sell cnt:", sell_cnt, "pos day:", position_day_cnt, "pos day rate:",
-              position_day_rate, "average pos day:", position_day_cnt / sell_cnt)
+              position_rate, "average pos day:", position_day_cnt / sell_cnt)
         return self.trade_history
 
 
 if __name__ == '__main__':
+    filepath = BASE_PATH + "/data/eth_usd_20230710.csv"
+    # filepath = BASE_PATH + "/data/eth_usd_polygon_20230714.csv"
+
     init_usd = 3000
-    trade_amount = 1
+    trade_amount = 0.5
     trading_fee = 0.0006
-    tipping_point_strategy = TippingPointStrategy(init_usd, trade_amount, trading_fee)
+    tipping_point_strategy = TippingPointStrategy(init_usd, trade_amount, trading_fee, filepath, False)
 
     period = 15
     trade_history = tipping_point_strategy.main(period)
-    print("trade history:", trade_history)
+    # print("trade history:", trade_history)
--- src/data_parser/price_data_parser.py
+++ src/data_parser/price_data_parser.py
@@ -18,16 +18,16 @@ def parse_yahoo_data(file_path, start_datetime="0000-00-00", end_datetime=None):
     return data
 
 
-def parse_polygon_data(file_path, start_datetime, end_datetime=None):
+def parse_polygon_data(file_path, start_datetime="0000-00-00 00:00:00", end_datetime=None):
     df = pd.read_csv(file_path)
     records = df.to_dict("records")
     data = []
     for item in records:
-        datetime = utils.utc_timestamp_to_datetime_ms_str(item["t"])
+        datetime = utils.utc_timestamp_to_datetime_ms_str(item["t"])[:-4]
         if (end_datetime is None and start_datetime <= datetime) or (
                 end_datetime is not None and start_datetime <= datetime <= end_datetime):
             item_dict = dict()
-            item_dict["datetime"] = datetime[:-4]
+            item_dict["datetime"] = datetime
             item_dict["price"] = float(item["o"])
             data.append(item_dict)
     return data
