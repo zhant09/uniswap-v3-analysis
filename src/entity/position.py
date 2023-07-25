04947e03a138f4721a3ced6b85b2856522d43f18
zhant09 <zhantao92@gmail.com>
2023-07-25 22:51:24 +0800

    add general position

--- src/entity/position.py
+++ src/entity/position.py
@@ -60,3 +60,69 @@ class Position(object):
 
     def __str__(self):
         return "current_eth: {}, current_usd: {}".format(self.current_eth, self.current_usd)
+
+
+class GeneralPosition(object):
+
+    def __init__(self, token0, token1, token0_amount, token1_amount, trading_fee):
+        self._token0 = token0
+        self._token1 = token1
+        self._init_token0_amount = token0_amount
+        self._init_token1_amount = token1_amount
+        self._current_token0_amount = self._init_token0_amount
+        self._current_token1_amount = self._init_token1_amount
+        self.trading_fee = trading_fee
+
+    @property
+    def token0(self):
+        return self._token0
+
+    @property
+    def token1(self):
+        return self._token1
+
+    @property
+    def init_token0_amount(self):
+        return self._init_token0_amount
+
+    @property
+    def init_token1_amount(self):
+        return self._init_token1_amount
+
+    @property
+    def current_token0_amount(self):
+        return self._current_token0_amount
+
+    @current_token0_amount.setter
+    def current_token0_amount(self, value):
+        self._current_token0_amount = value
+
+    @property
+    def current_token1_amount(self):
+        return self._current_token1_amount
+
+    @current_token1_amount.setter
+    def current_token1_amount(self, value):
+        self._current_token1_amount = value
+
+    # price is token0 to token1 price
+    def buy_token0(self, price, amount):
+        if self._current_token1_amount < amount * price * (1 + self.trading_fee):
+            raise ValueError("not enough token1 to buy token0")
+
+        self._current_token0_amount += amount
+        self._current_token1_amount -= amount * price * (1 + self.trading_fee)
+
+    def sell_token0(self, price, amount):
+        if self._current_token0_amount < amount:
+            raise ValueError("not enough token0 to sell")
+
+        self._current_token0_amount -= amount
+        self._current_token1_amount += amount * (1 - self.trading_fee) * price
+
+    def get_profit(self, price):
+        return self._current_token1_amount + self._current_token0_amount * price - self._init_token1_amount - \
+            self._init_token0_amount * price
+
+    def get_profit_rate(self, price):
+        return self.get_profit(price) / (self._init_token1_amount + self._init_token0_amount * price)
